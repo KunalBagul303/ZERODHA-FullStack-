8474b4b077765f723939b2e0bbb8b43fc4007396
KunalBagul303 <kunalbagul315@gmail.com>
2026-01-30 22:46:34 +0530

    final rightsection

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import Hero from "./Hero";
 import LeftSection from "./LeftSection";
-import RightSection from "./RlghtSection";
+import RightSection from "./RightSection";
 import Universe from "./Universe";
 
 import Navbar from "../Navbar";
